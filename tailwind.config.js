9caef0b7861cb0a64a3d75afbc16721e86ec5d3d
Duy Pham <ericpham19@outlook.com>
2022-11-14 21:22:10 +1100

    updated config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 
-  darkMode: "class",
+  
   content: [
     "./pages/**/*.[js,jsx,html]",
    
@@ -10,4 +10,5 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  darkMode: "class",
 }
